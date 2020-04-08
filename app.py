e9753ec838c22b7533ff4cb312bd4afa5bd94e74
vang1ong7ang <vang1ong7ang@outlook.com>
2020-04-08 07:35:14 +0000

    update import

--- app.py
+++ app.py
@@ -1,32 +1,32 @@
-import libsm
-import logging
+from libsm import Value, SM
+from logging import basicConfig, INFO
 
 # set logging level
-logging.basicConfig(level=logging.INFO)
+basicConfig(level=INFO)
 
 
 # fi: init function
-#     init function returns a set (libsm.Value) of initial state
+#     init function returns a set (Value) of initial state
 #     the only initial state in this example is `{}`
 
 
 def fi():
-    return libsm.Value(
-        libsm.Value(),
+    return Value(
+        Value(),
     )
 
 # fn: next function
-#     next function returns a set (libsm.Value) of next state
+#     next function returns a set (Value) of next state
 #     the only next state in this example `{}` whatever the previous state is
 
 
 def fn(value):
-    return libsm.Value(
-        libsm.Value(),
+    return Value(
+        Value(),
     )
 
 
-sm = libsm.SM(fi, fn)
+sm = SM(fi, fn)
 states, transitions = sm.result()
 
 print(states)
